36d941756fe438147d9191ff2215f902a8551a30
AhmedAliAhmedMostafa <ahmed_elady42@yahoo.com>
2019-03-16 21:17:53 +0200

    Fix bugs in DIO_ReadPort and Port_SetPinPullUp.

--- Source Files/DIO_ReadPort.c
+++ Source Files/DIO_ReadPort.c
@@ -6,8 +6,14 @@
 #define GPIO_PORTF_DATA_R       (*((volatile unsigned long *)0x400253FC))
 unsigned char  DIO_ReadPort (unsigned char port_index,unsigned char pins_mask) 
 {
-	long int all[6]={GPIO_PORTA_DATA_R,GPIO_PORTB_DATA_R ,GPIO_PORTC_DATA_R ,GPIO_PORTD_DATA_R,GPIO_PORTE_DATA_R,GPIO_PORTF_DATA_R};
+volatile unsigned long * all[6]={
+	(volatile unsigned long *)0x400043FC,
+	(volatile unsigned long *)0x400053FC,
+(volatile unsigned long *)0x400063FC,
+(volatile unsigned long *)0x400073FC,
+(volatile unsigned long *)0x400243FC,
+(volatile unsigned long *)0x400253FC};
 	
-		return (all[port_index]&pins_mask);
+		return ((*(all[port_index]))&pins_mask);
 	
 }
--- Source Files/Port_SetPinPullUp.c
+++ Source Files/Port_SetPinPullUp.c
@@ -6,12 +6,17 @@
 #define GPIO_PORTF_PUR_R        (*((volatile unsigned long *)0x40025510))
 void Port_SetPinPullUp (unsigned char port_index,unsigned char pin_mask,unsigned char enable) 
 {
-	long int all[6]={GPIO_PORTA_PUR_R,GPIO_PORTB_PUR_R ,GPIO_PORTC_PUR_R ,GPIO_PORTD_PUR_R,GPIO_PORTE_PUR_R,GPIO_PORTF_PUR_R};
+	volatile unsigned long int * all[6]={(volatile unsigned long *)0x40004510,
+(volatile unsigned long *)0x40005510,
+(volatile unsigned long *)0x40006510,
+(volatile unsigned long *)0x40007510,
+(volatile unsigned long *)0x40024510,
+(volatile unsigned long *)0x40025510};
 	switch(enable)
 	{
-		case '0':all[port_index]&=~pin_mask;
+		case '0':(*(all[port_index]))&=~pin_mask;
 			break;
-		case '1': all[port_index]|=pin_mask;
+		case '1': (*(all[port_index]))|=pin_mask;
 			break;
 	}
 
