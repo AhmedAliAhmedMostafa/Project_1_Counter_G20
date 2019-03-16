dfe2e595c89f1a129ae843565ddb8bd6caf79a5d
emptyContent <36591134+emptyContent@users.noreply.github.com>
2019-03-17 00:48:49 +0200

    implements:: Phase I_module-B_B.4

---
+++ Source Files/Port_SetPinPullDown.c
@@ -0,0 +1,27 @@
+
+#include "stdint.h"
+/* pre-condition:: an array of Pull-down registers for ALL ports && enum for ports*/
+/********************************************************************/
+
+#define GPIO_PORTA_PDR_R        (*((volatile uint32_t *)0x40004514))
+#define GPIO_PORTB_PDR_R        (*((volatile uint32_t *)0x40005514))
+#define GPIO_PORTC_PDR_R        (*((volatile uint32_t *)0x40006514))
+#define GPIO_PORTD_PDR_R        (*((volatile uint32_t *)0x40007514))
+#define GPIO_PORTE_PDR_R        (*((volatile uint32_t *)0x40024514))
+#define GPIO_PORTF_PDR_R        (*((volatile uint32_t *)0x40025514))
+
+typedef enum ports{PA,PB,PC,PD,PE,PF} selPort ;
+
+
+void Port_SetPinPullDown(uint8_t port_index ,uint8_t pins_mask , uint8_t enable){
+uint32_t PDR[] ={GPIO_PORTA_PDR_R,GPIO_PORTB_PDR_R ,GPIO_PORTC_PDR_R,GPIO_PORTD_PDR_R,GPIO_PORTE_PDR_R ,GPIO_PORTF_PDR_R} ;
+selPort pt =port_index  ;
+
+if(enable)
+PDR[pt] |=pins_mask ; //assuming that the nth pin position = 1 means selected.
+
+else 
+PDR[pt] ^=pins_mask ;
+
+
+}
